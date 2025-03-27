d905c6bb2d8a7243b18ba2c82cba618fd162dce3
Francisco Welio <fwelio.dev@gmail.com>
2025-03-27 15:27:48 -0300

    feat/tests-init

---
+++ sistemaacademico/src/test/java/AlunoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.sistemaacademico.Aluno;
+public class AlunoTest {
+    
+}
